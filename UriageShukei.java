1bb8dfdcc55f988e92fe5e648683a72d1aaa45a0
ikedahiroaki <goike042815@gmail.com>
2016-02-22 12:03:53 +0900

    UriageShukei.java
    追加

--- UriageShukei.java
+++ UriageShukei.java
@@ -1,92 +1,108 @@
 ﻿package UriageShukei;
 
 import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+
+ class UriageShukei {
+	public static void main(String []args){
+
+		// 指定されたディレクトリ
+		File file = new File(args[0]);
 
 
-public class UriageShukei{
+
+		// 1.支店定義ファイル
+
+	   file = new File("\\branch.lst");
 
 
-	public static void main(String[] args) throws IOException{
-		//指定されたディレクトリ
-		File directory = new File(args [0]);
-
-		//1.支店定義ファイル
-
-		File branchFile = new File(directory + "\\branch.lst");
-
-		//変数の宣言
-		FileInputStream fis = new FileInputStream(branchFile);
-		InputStreamReader isr = new InputStreamReader(fis);
+		FileInputStream fis = new FileInputStream(file);
+		InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
 		BufferedReader br = new BufferedReader(isr);
-		ArrayList<String []> branchData = new ArrayList<String []>();
-		//支店定義の処理
-
-			if(!branchFile.exists()) {
-				System.out.println("支店定義ファイルが存在しません");
-				return;
-			}
 
 
 
 
+		// 支店定義の処理
 
-		fis = new FileInputStream(branchFile);
-		isr = new InputStreamReader(fis);
+		if (!file.exists()) {
+			System.out.println("支店定義ファイルが存在しません");
+			return;
+		}
+
+		fis = new FileInputStream(file);
+		isr = new InputStreamReader(fis,"UTF-8");
 		br = new BufferedReader(isr);
 		String line;
-		while((line = br.readLine()) != null){
+
+		while ((line = br.readLine()) != null) {
 			String[] branch = line.split(",");
-		//配列の長さ
-			if(branch.length != 2 || branch [0] == null || branch [1] == null){
-				System.out.println("支店定義ファイルのフォーマットが不正です");
+			// 配列の長さ
+
+			System.out.println("支店定義ファイルのフォーマットが不正です");
 				return;
-			}
+
 
 		}
 
+		// 2.商品定義ファイル
+
+		File commodityFile = new File("\\commodity.lst");
 
 
-		//2.商品定義ファイル
 
-		File commodityFile = new File(directory + "\\commodity.lst");
-		ArrayList<String[]> commodityData = new ArrayList<String[]>();
+		FileInputStream fs = new FileInputStream(commodityFile);
+		InputStreamReader ir = new InputStreamReader(fs,"UTF-8");
+		BufferedReader b = new BufferedReader(ir);
 
-		//商品定義の処理
-		if(!commodityFile.exists()){
+		//HashMap<String, String> commondity = new HashMap<String, String>();
+
+
+		// 商品定義の処理
+		if (!commodityFile.exists()) {
 			System.out.println("商品定義ファイルは存在しません");
 			return;
 		}
 
-		fis = new FileInputStream(commodityFile);
-		isr = new InputStreamReader(fis);
-		br = new BufferedReader(isr);
+		fs = new FileInputStream(commodityFile);
+		ir = new InputStreamReader(fs,"UTF-8");
+		b = new BufferedReader(ir);
 
-		while((line = br.readLine()) != null){
-			String [] commodity = line.split(",");
-		//配列の長さ
-			if(commodity.length!= 2 || commodity [0] == null || commodity [1] == null ){
+		while ((line = br.readLine()) != null) {
+			String[] commodity = line.split(",");
+			// 配列の長さ
+
 				System.out.println("商品定義ファイルのフォーマットが不正です");
 				return;
 			}
 		}
 
+		// 3.集計
 
+		// .rcdに該当する拡張子を検索
+		// HashMap<String, String> salesData = new HashMap<String, String>();
 
-		//3.集計
-
-		//.rcdに該当する拡張子を検索
-		ArrayList<String[]> salesData = new ArrayList<String[]>();
 		String[] salesFiles = directory.list();
-			for (String salesFile : salesFiles) {
-				if (!salesFile.endsWith(".rcd")) {
-				continue;
+		for (String salesFile : salesFiles) {
+			if (!salesFile.endsWith(".rcd")) {
+				System.out.println("売上ファイル名が連番になっていません");
+				return;
+
+			}
+
+			File rcdFile = new File("\\" + salesFile);
+			fis = new FileInputStream(rcdFile);
+			isr = new InputStreamReader(fis);
+			br = new BufferedReader(isr);
+
+			String branch = br.readLine();
+			String salesCommodity = br.readLine();
+			String price = br.readLine();
+
 		}
 	}
 }
-
-
